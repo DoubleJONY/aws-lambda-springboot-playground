3f7a6086b71fe73ec18f107434bc0c4b724b1a92
JONY <jony@mail.i-on.net>
2022-01-17 15:57:12 +0900

    WIP: Added AWS Lambda Handler and gradle script for deployment

---
+++ src/main/java/com/example/springstarter/handler/LambdaHandlers.java
@@ -0,0 +1,7 @@
+package com.example.springstarter.handler;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootStreamHandler;
+
+public class LambdaHandlers extends SpringBootStreamHandler {
+
+}
